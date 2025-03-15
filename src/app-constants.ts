dc070b5f0a58487409559498ce0a7d3de42f9c77
Muthaiyyan <ephrash@yahoo.com>
2025-03-15 12:07:53 +0530

    Update app-constants.ts

--- src/app-constants.ts
+++ src/app-constants.ts
@@ -1,4 +1,5 @@
 export class AppConstants{
 
-    static BACKEND_API_URL= 'http://3.110.33.27:8081/products';
+    //static BACKEND_API_URL= 'http://3.110.33.27:8081/products';
+  static BACKEND_API_URL= 'a2ad5fb0835b749c3823afb7bec99d8e-317652449.us-east-1.elb.amazonaws.com'
 }
